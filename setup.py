3c47916913f588793284bd5c0246ce33304bc2b3
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-19 18:23:27 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+ name="django-track",
+ version="0.1.0",
+ description="A django app for doing analytics on site usage.",
+ url="https://track.samireland.com",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: SDevelopers",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: System :: Logging",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+  "Programming Language :: Python :: 3.7",
+ ],
+ keywords="django analytics logging",
+ packages=["track"],
+ install_requires=[]
+)
