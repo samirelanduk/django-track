f3a5d952e770533cb0e014ec378321c2058684f6
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-20 11:10:13 +0100

    Histogram displays

--- track/models.py
+++ track/models.py
@@ -1,5 +1,11 @@
 
+import os
+from datetime import datetime, timedelta
+import pytz
 from django.db import models
+from django.contrib.gis.geoip2 import GeoIP2
+
+DL = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/track/data"
 
 class Visit(models.Model):
 
@@ -10,3 +16,38 @@ class Visit(models.Model):
     path = models.CharField(max_length=256, blank=True, null=True)
     IP = models.CharField(max_length=64, blank=True, null=True)
     referer = models.CharField(max_length=256, blank=True, null=True)
+
+    country_lookups = {}
+    city_lookups = {}
+
+    @staticmethod
+    def from_day(dt):
+        start = datetime(
+         dt.year, dt.month, dt.day, 0, 0, 0, tzinfo=dt.tzinfo
+        ).astimezone(pytz.UTC).replace(tzinfo=None)
+        end = start + timedelta(days=1)
+        return Visit.objects.filter(datetime__gt=start, datetime__lt=end)
+
+
+    @property
+    def country(self):
+        try:
+            return self.country_lookups[self.IP]
+        except:
+            try:
+                country = GeoIP2(path=DL).country(self.IP)["country_name"]
+            except: country = "Unknown"
+            self.country_lookups[self.IP] = country
+            return country
+
+
+    @property
+    def city(self):
+        try:
+            return self.city_lookups[self.IP]
+        except:
+            try:
+                city = GeoIP2(path=DL).city(self.IP)["city"]
+            except: city = "Unknown"
+            self.city_lookups[self.IP] = city
+            return city
--- track/views.py
+++ track/views.py
@@ -1,5 +1,25 @@
+from datetime import datetime, date
+import pytz
+from collections import Counter
 from django.shortcuts import render
 from .models import Visit
 
 def analytics(request):
-    return render(request, "track/analytics.html", {"visits": Visit.objects.all()})
+    TZ = "Pacific/Auckland"
+
+    timezone = pytz.timezone(TZ)
+    now = datetime.now(timezone)
+    visits = list(Visit.from_day(now))
+    for v in visits:
+        v.time = (v.datetime + now.tzinfo.utcoffset(now)).time()
+
+    histograms = [
+     (sorted(dict(Counter([getattr(v, f) for v in visits])).items(),
+      key=lambda r: r[1], reverse=True), f) for f in ["path", "country", "city"]
+    ]
+
+    return render(request, "track/analytics.html", {
+     "date": now.date(),
+     "visits": visits,
+     "histograms": histograms
+    })
