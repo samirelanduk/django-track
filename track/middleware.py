c1eb6006e8e3dd40a119beb25a92b1b23e927d5e
Sam Ireland <sam.ireland.uk@gmail.com>
2018-09-20 11:26:36 +0100

    Add URL exclusion

--- track/middleware.py
+++ track/middleware.py
@@ -1,22 +1,29 @@
 from datetime import datetime
+import re
 from django.conf import settings
+from django.urls import reverse
 from .models import Visit
+from .views import analytics
 
 def inspect_response(get_response):
 
-    def middleware(request):
-        track = True
-        try:
-            if request.path.startswith("/" + settings.MEDIA_URL): track = False
-        except: pass
+    exclude = settings.__dict__.get("TRACK_EXCLUDE", [])
+    exclude += [reverse(analytics)]
 
+    def middleware(request):
+        track = not any(re.search(p, request.path) for p in exclude)
         if track:
-            Visit.objects.create(
-             datetime=datetime.utcnow(),
-             path=request.path,
-             IP=request.META.get("REMOTE_ADDR"),
-             referer=request.META.get("HTTP_REFERER")
-            )
+            try:
+                if request.path.startswith("/" + settings.MEDIA_URL): track = False
+            except: pass
+
+            if track:
+                Visit.objects.create(
+                 datetime=datetime.utcnow(),
+                 path=request.path,
+                 IP=request.META.get("REMOTE_ADDR"),
+                 referer=request.META.get("HTTP_REFERER")
+                )
 
         response = get_response(request)
         return response
